f2d024e400826180719f87567d7bb413c66857c9
George <georgelanes@gmail.com>
2019-10-21 02:35:55 -0300

    Add Index on ProjectController

--- src/app/controllers/ProjectController.js
+++ src/app/controllers/ProjectController.js
@@ -3,6 +3,10 @@ class ProjectController {
     const { id, title } = req.body;
     return res.json({ data: req.body });
   }
+
+  async index(req, res) {
+    return res.json({ message: 'Project List' });
+  }
 }
 
 export default new ProjectController();
